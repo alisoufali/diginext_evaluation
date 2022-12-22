6563f6e0d86bba970cc383e878057c812e3c151f
alisufali <alisufalipetroleum@gmail.com>
2022-12-22 23:09:16 +0330

    Added question 1 function

---
+++ question1.py
@@ -0,0 +1,10 @@
+def function(string: str, number: int) -> int:
+    number_repeats = len(string) // number
+    number_remaining = len(string) % number
+    repeated_string: str = ""
+    for _ in range(number_repeats):
+        repeated_string += string
+    else:
+        repeated_string += string[:number_remaining]
+    number_a_char = repeated_string.count("a")
+    return number_a_char
