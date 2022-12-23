9a8c23a4a3d6a27820121cfe1d742ce459d5b2b1
alisufali <alisufalipetroleum@gmail.com>
2022-12-23 15:03:30 +0330

    Debugged question1 solution

--- question1.py
+++ question1.py
@@ -1,10 +1,24 @@
+import sys
+
+
+class InputManager:
+
+    @staticmethod
+    def get_and_convert_input_data() -> tuple[str, int]:
+        input_data = sys.argv[1]
+        string, number = input_data.split(",")
+        number = int(number)
+        converted_input_data = (string, number)
+        return converted_input_data
+
+
 class Utils:
 
     @staticmethod
     def repeat_string_to_a_number(string: str, number: int) -> str:
         string_length = len(string)
-        number_complete_repeats = string_length // number
-        length_remaining_string = string_length % number
+        number_complete_repeats = number // string_length
+        length_remaining_string = number % string_length
         created_string = ""
         for _ in range(number_complete_repeats):
             created_string += string
@@ -23,10 +37,23 @@ class Utils:
         return number_occurances
 
 
-# This is the function used to solve the problem
-def count_a_chars_in_repeated_string(string: str, number: int) -> int:
-    created_string = Utils.repeat_string_to_a_number(
+class Main:
+    # This is the function used to solve the problem
+    @staticmethod
+    def count_a_chars_in_repeated_string(string: str, number: int) -> int:
+        created_string = Utils.repeat_string_to_a_number(
+            string=string, number=number)
+        number_occurances_a_character = Utils.count_character_in_string(
+            string=created_string, character="a")
+        return number_occurances_a_character
+
+
+if __name__ == "__main__":
+
+    # string, number = InputManager.get_and_convert_input_data()
+    string, number = "aba", 10
+    number_occurances_a_character = Main.count_a_chars_in_repeated_string(
         string=string, number=number)
-    number_occurances_a_character = Utils.count_character_in_string(
-        string=created_string, character="a")
-    return number_occurances_a_character
+    print(f"For input string: {string} and number: {number}, "
+          "the number of a character occurances in string = "
+          f"{number_occurances_a_character}")
